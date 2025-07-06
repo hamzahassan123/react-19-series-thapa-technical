d86bb7b3999bd6c52703cea5bbf526e0a3d01df1
Hamza Hassan <hamza140934@gmail.com>
2025-07-06 18:29:48 +0500

    netflix compenets added - fetch data from api/json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 //subscribe to Thapa Technical Youtube Channel - https://www.youtube.com/thapatechnical
 
+import NetflixCard from "./components/NetfilxCard";
 import { ReactMemo } from "./hooks/Memo/ReactMemo";
 
 export const App = () => {
   return (
     <>
-      <ReactMemo />
+      <NetflixCard />
     </>
   );
 };
---
+++ src/components/NetflixCards2.jsx
@@ -0,0 +1,10 @@
+import seriesData from '../api/seriesData.json'
+
+const NetfilxCards2 = () => {
+    return (
+        <>
+
+        </>
+    )
+}
+export default NetfilxCards2;
