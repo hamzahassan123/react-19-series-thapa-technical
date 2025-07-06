45da0241664df62504dd5f13174649196e4a8e09
Hamza Hassan <hamza140934@gmail.com>
2025-07-06 19:16:41 +0500

    card component is completed with css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 //subscribe to Thapa Technical Youtube Channel - https://www.youtube.com/thapatechnical
 
 import NetflixCard from "./components/NetfilxCard";
+import NetfilxCards2 from "./components/NetflixCards2";
 import { ReactMemo } from "./hooks/Memo/ReactMemo";
 
 export const App = () => {
   return (
     <>
-      <NetflixCard />
+      {/* <NetflixCard /> */}
+      <NetfilxCards2 />
     </>
   );
 };
--- src/components/NetflixCards2.jsx
+++ src/components/NetflixCards2.jsx
@@ -1,10 +1,32 @@
 import seriesData from '../api/seriesData.json'
+import './NetflixCard.css';
 
 const NetfilxCards2 = () => {
+    const age = 44;
     return (
         <>
+            {/* <input type="number" placeholder='enter your age' value={age} onChange={(e) => setAge(Number(e.target.value))} /> */}
+            <ul>
+                {
+                    seriesData.map((e) => {
+                        return (<li key={e.id}>
+                            <div>
+                                <img src={e.img_url} alt="card image" width='40%' height='40%' />
+                            </div>
+                            <h2>Name: {e.name}</h2>
+                            <h2>Rating: {e.rating} </h2>
+                            <p>Summary: {e.description} </p>
+                            <p>Genre: {e.genre} </p>
+                            {age >= 16 ? <button><a href={e.watch_url} target="_blank" rel="noopener noreferrer">Watch Now</a></button> : <button disabled>Age Restricton</button>}
+                        </li>)
+                    })
 
+                }
+
+            </ul>
         </>
     )
 }
 export default NetfilxCards2;
+
+
