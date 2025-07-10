a3db5c0c7b141bc288bcea36dfb9687a9e238295
Hamza Hassan <hamza140934@gmail.com>
2025-07-11 00:28:06 +0500

    facing error in tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-//subscribe to Thapa Technical Youtube Channel - https://www.youtube.com/thapatechnical
-
-import NetflixCard from "./components/NetfilxCard";
+// import NetflixCard from "./components/NetfilxCard";
 import NetfilxCards2 from "./components/NetflixCards2";
-import { ReactMemo } from "./hooks/Memo/ReactMemo";
+// import { ReactMemo } from "./hooks/Memo/ReactMemo";
 
 export const App = () => {
   return (
--- src/components/NetflixCards2.jsx
+++ src/components/NetflixCards2.jsx
@@ -1,26 +1,15 @@
 import seriesData from '../api/seriesData.json'
-import './NetflixCard.css';
+// import './NetflixCard.css';
+import NetflixCardContent from './NetflixCardContent';
 
 const NetfilxCards2 = () => {
-    const age = 44;
     return (
         <>
-            {/* <input type="number" placeholder='enter your age' value={age} onChange={(e) => setAge(Number(e.target.value))} /> */}
             <ul>
                 {
-                    seriesData.map((e) => {
-                        return (<li key={e.id}>
-                            <div>
-                                <img src={e.img_url} alt="card image" width='40%' height='40%' />
-                            </div>
-                            <h2>Name: {e.name}</h2>
-                            <h2>Rating: {e.rating} </h2>
-                            <p>Summary: {e.description} </p>
-                            <p>Genre: {e.genre} </p>
-                            {age >= 16 ? <button><a href={e.watch_url} target="_blank" rel="noopener noreferrer">Watch Now</a></button> : <button disabled>Age Restricton</button>}
-                        </li>)
+                    seriesData.map((currElem) => {
+                        return (<NetflixCardContent key={currElem.id} currElem={currElem} />)
                     })
-
                 }
 
             </ul>
